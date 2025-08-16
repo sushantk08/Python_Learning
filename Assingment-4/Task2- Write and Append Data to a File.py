7db0f586def4970c8b712a7f475d423b0c81538e
sushant kulkarni <kulkarnisushant87@gmail.com>
2025-08-16 16:19:07 +0530

    changes in task2

--- Assingment-4/Task2- Write and Append Data to a File.py
+++ Assingment-4/Task2- Write and Append Data to a File.py
@@ -1,15 +1,15 @@
-f=open("output.txt",'w')
+f=open("E:\Python Course with Notes\Python\Python_Learning\Assingment-4\output.txt",'w')
 string=input("Enter the text to write in a file: ")
 f.write(string+"\n")
 print("Data sucessfully written to output.txt\n")
 
 
-f=open("output.txt",'a')
+f=open("E:\Python Course with Notes\Python\Python_Learning\Assingment-4\output.txt",'a')
 string2=input("Enter additional text to write in a file: ")
 f.write(string2)
 print("Data sucessfully appended\n")
 print("final content of output.txt: ")
 
-f=open("output.txt",'r')
+f=open("E:\Python Course with Notes\Python\Python_Learning\Assingment-4\output.txt",'r')
 print(f.read())
 
